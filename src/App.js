ea1d8d4b5f822ed1d1a16f3a20621f06bc3baca0
Pranay Upputuri <pranay6680@gmail.com>
2024-02-09 09:38:26 +0000

    formik

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useFormik } from 'formik';
+
 
 function App() {
+  const pran = useFormik({
+    // fixed way writing intialvalues
+    initialValues:{
+      name:'',
+      email:'',
+      password:"" 
+    },
+    //to make changes in intitalvalues we have to write below code [onsubmit is predefined hook in formik]
+    onSubmit:(values)=>{
+     console.log('onsubmit',values)
+    },
+    //to validate errors use below code [validate is a predefined hook in formik]
+    validate:(values)=>{       //[Here values takes the initialvalues properties]
+    let errors = {};           //[Assigned a empty braces to display error message]
+  console.log("console",values)
+      if(!values.name){
+        errors.name= "Name required"
+      }
+      if(!values.email){
+        errors.email= "email required"
+      }
+      if(!values.password){
+        errors.password= "password required"
+      }
+      return errors;
+    }
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={pran.handleSubmit}>
+        <label>Name:</label>
+        <input type="text" name="name" value={pran.values.name} onChange={pran.handleChange} onBlur={pran.handleBlur}/> 
+        {/* Here onChange/handleChange , onBlur/handleBlur are the inbuuilt formik function to handle events */}
+        {pran.errors.name?<div>{pran.errors.name}</div>:null}
+        <br/>
+        <label>Email:</label>
+        <input type="email" name="email" value={pran.values.email} onChange={pran.handleChange}/>
+        {pran.errors.email?<div>{pran.errors.email}</div>:null}
+        <br/>
+        <button type="submit" >Submit</button>
+      </form>
     </div>
   );
 }
